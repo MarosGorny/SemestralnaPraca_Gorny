c6f76fb0717554de69b53c2cc5d2a96a73c5dd54
MarosG <MarosGorny@gmail.com>
2022-06-12 14:42:23 +0200

    add timePickerDialog, datePickerDialog

--- TrainingLog/app/src/main/java/com/example/traininglog/gorny/traininglog/addActivity/AddActivityLogActivity.kt
+++ TrainingLog/app/src/main/java/com/example/traininglog/gorny/traininglog/addActivity/AddActivityLogActivity.kt
@@ -1,8 +1,12 @@
 package com.example.traininglog.gorny.traininglog.addActivity
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -11,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 import com.example.traininglog.gorny.traininglog.data.EnumAktivity
 import com.example.traininglog.gorny.traininglog.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val TRAINING_LOG_DISTANCE = "distance"
 const val LOG_ACTIVITY = "log activity"
@@ -27,23 +33,41 @@ class AddActivityLog: AppCompatActivity() {
 
         findViewById<Button>(R.id.dateButton).setOnClickListener {
             //TODO spravit aby som mohol vyklikat datum
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener { datePicker,year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                findViewById<Button>(R.id.dateButton).text = SimpleDateFormat("dd.MM.yy").format(cal.time)
+            }
+            DatePickerDialog(this,dateSetListener,cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
         }
 
         findViewById<Button>(R.id.timeButton).setOnClickListener {
             //TODO spravit aby som si mohol vyklikat cas
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                findViewById<Button>(R.id.timeButton).text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
         }
 
         findViewById<Button>(R.id.button_done).setOnClickListener {
+
+            val selectedId = findViewById<RadioGroup>(R.id.activity_options).checkedRadioButtonId
+            logType = when (selectedId) {
+                R.id.option_run -> "Run"
+                R.id.option_bike -> "Bike"
+                else -> "Swim"
+            }
+
             addTrainingLog()
         }
 
         addDistance = findViewById(R.id.distance_input_number)
-        val selectedId = findViewById<RadioGroup>(R.id.activity_options).checkedRadioButtonId
-        logType = when (selectedId) {
-            R.id.option_run -> "Run"
-            R.id.option_bike -> "Bike"
-            else -> "Swim"
-        }
+
     }
 
 
@@ -64,4 +88,5 @@ class AddActivityLog: AppCompatActivity() {
         }
         finish()
     }
+
 }
