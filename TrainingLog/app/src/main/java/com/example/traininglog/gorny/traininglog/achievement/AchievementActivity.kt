c9bc298d9436c00bd6c81709f43837d686ee271c
MarosG <MarosGorny@gmail.com>
2022-06-14 01:00:16 +0200

    add fragment

--- TrainingLog/app/src/main/java/com/example/traininglog/gorny/traininglog/achievement/AchievementActivity.kt
+++ TrainingLog/app/src/main/java/com/example/traininglog/gorny/traininglog/achievement/AchievementActivity.kt
@@ -2,9 +2,14 @@ package com.example.traininglog.gorny.traininglog.achievement
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import com.example.traininglog.gorny.traininglog.R
 
 class AchievementActivity :AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    //private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.achievements_activity)
