5e0eb779ff84b4c7bb05cc20987bd1a31998c4d3
MarosG <MarosGorny@gmail.com>
2022-06-14 02:16:19 +0200

    crash when opening second activity

--- TrainingLog/app/src/main/java/com/example/traininglog/gorny/traininglog/achievement/AchievementActivity.kt
+++ TrainingLog/app/src/main/java/com/example/traininglog/gorny/traininglog/achievement/AchievementActivity.kt
@@ -3,15 +3,39 @@ package com.example.traininglog.gorny.traininglog.achievement
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.traininglog.gorny.traininglog.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AchievementActivity :AppCompatActivity() {
 
     private lateinit var navController: NavController
-    //private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.achievements_activity)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.host_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.achievement_run_list)
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
+
